fd7bb46a70b5d264fb026eab19cbdb7e0991bdd1
Robert McCarter <69922179+robertmc5@users.noreply.github.com>
2023-04-13 23:21:06 -0400

    feat: create container and component structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
 
+import { Blog, Features, Footer, Header, Possibility, WhatGPT3 } from './containers';
+import { Brand, CTA, Navbar } from './components';
+import './App.css';
+
 const App = () => {
   return (
-    <h1>GPT-3</h1>
+    <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
   );
 }
 
